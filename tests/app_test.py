02afcc90a4f8fc75aa3e6a3efa5005f9969515dd
kyle hiroyasu <kylehiroyasu@gmail.com>
2020-10-27 22:56:39 +0100

    tests for login failing as expected

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,7 +1,32 @@
+import pytest
+import os
 from pathlib import Path
 
 from project.app import app, init_db
 
+TEST_DB = "test.db"
+
+@pytest.fixture
+def client():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    app.config['TESTING'] = True
+    app.config['DATABASE'] = BASE_DIR.joinpath(TEST_DB)
+
+    init_db() #setup
+    yield app.test_client() #tests get run here
+    init_db() #teardown
+
+def login(client, username, password):
+    """ Login helper"""
+    return client.post(
+        "/login",
+        data=dict(username=username, password=password),
+        follow_redirects=True,
+    )
+
+def logout(client):
+    """ Logout helper """
+    return client.get("/logout", follow_redirects=True)
 
 #testing that the basic index page works
 def test_index():
@@ -16,3 +41,29 @@ def test_index():
 def test_database():
     init_db()
     assert Path("flaskr.db").is_file()
+
+def test_empty_db(client):
+    """Ensure database is blank"""
+    rv = client.get("/")
+    assert b"No entries yet. Add some!" in rv.data
+
+def test_login_logout(client):
+    rv = login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    assert b"You were logged in" in rv.data
+    rv = logout(client)
+    assert b"You were logged out" in rv.data
+    rv = login(client, app.config["USERNAME"]+"x", app.config["PASSWORD"])
+    assert b"Invalid username" in rv.data
+    rv = login(client, app.config["USERNAME"], app.config["PASSWORD"]+'x')
+    assert b"Invalid password" in rv.data
+
+def test_messages(client):
+    login(client, app.config['USERNAME'], app.config['PASSWORD'])
+    RV = CLIENT.POST(
+        "/add",
+        data=dict(title="<Hello>", text="<string>HTML</strong> allowed here"),
+        follow_redirects=True,
+    )
+    assert b"No entries here so far" not in rv.data
+    assert b"&lt;Hello&gt;" in rv.data   
+    assert b"<strong>HTML</strong> allowed here" in rv.data   
