00246798f04b51f0bd285f7096b1115c86be34b8
kyle hiroyasu <kylehiroyasu@gmail.com>
2020-10-28 18:24:26 +0100

    adding endpoint to save data and styling

--- project/app.py
+++ project/app.py
@@ -57,12 +57,25 @@ def login():
             return redirect(url_for('index'))
     return render_template('login.html', error=error)
 
-
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('index'))
+
+@app.route('/add', methods=['Post'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute(
+        'insert into entries (title, text) values (?, ?)',
+        [request.form['title'], request.form['text']]
+    )
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run()
